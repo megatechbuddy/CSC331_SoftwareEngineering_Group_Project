43a10aaca3fafd896188caf8e6d39f1d209aeb41
Sean Benson <seanalexanderbenson@gmail.com>
2017-09-24 17:01:27 -0500

    At tutorial https://www.youtube.com/watch?v=mCdnFa3U9vs&list=PLZm85UZQLd2SXQzsF-a0-pPF6IWDDdrXt&index=2 LibGDX - Super Mario Bros I made it use our team image that Chuyang made.

---
+++ AngryDonkeyKong/core/src/screens/PlayScreen.java
@@ -0,0 +1,64 @@
+//Author: Sean Benson 
+package screens;
+
+import com.angrydonkeykong.game.AngryDonkeyKongLibGDX;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class PlayScreen implements Screen{
+	private AngryDonkeyKongLibGDX game;
+	Texture img;
+	
+	public PlayScreen(AngryDonkeyKongLibGDX game) {
+		this.game = game;
+		img = new Texture("TitlePicture.jpg");
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.begin();
+		game.batch.draw(img, 0, 0);
+		game.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
