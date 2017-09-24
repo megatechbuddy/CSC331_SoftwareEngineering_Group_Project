a5c2f74cf8ef6eeea4ad517f025d1f9741d24724
Sean Benson <seanalexanderbenson@gmail.com>
2017-09-24 17:20:03 -0500

    I need help customizing this stable code to have the proper title image aspect ratio.
    -Sean Benson

--- AngryDonkeyKong/core/src/screens/PlayScreen.java
+++ AngryDonkeyKong/core/src/screens/PlayScreen.java
@@ -5,16 +5,25 @@ import com.angrydonkeykong.game.AngryDonkeyKongLibGDX;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PlayScreen implements Screen{
 	private AngryDonkeyKongLibGDX game;
 	Texture img;
+	private OrthographicCamera gamecam;
+	private Viewport gamePort;
 	
 	public PlayScreen(AngryDonkeyKongLibGDX game) {
 		this.game = game;
 		//This uses the image that Chuyang made.
 		img = new Texture("TitlePicture.jpg");
+		gamecam = new OrthographicCamera();
+		gamePort = new FitViewport(908, 590, gamecam);
 	}
 	
 	@Override
@@ -27,6 +36,7 @@ public class PlayScreen implements Screen{
 	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.setProjectionMatrix(gamecam.combined);
 		game.batch.begin();
 		game.batch.draw(img, 0, 0);
 		game.batch.end();
@@ -34,8 +44,7 @@ public class PlayScreen implements Screen{
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		gamePort.update(width, height);
 	}
 
 	@Override
