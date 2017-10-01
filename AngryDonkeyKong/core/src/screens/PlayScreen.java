cd6a650e12e61f781a9aa9a87b6bba677f96e75c
Sean Benson <seanalexanderbenson@gmail.com>
2017-10-01 18:03:03 -0500

    Fix this Keven

--- AngryDonkeyKong/core/src/screens/PlayScreen.java
+++ AngryDonkeyKong/core/src/screens/PlayScreen.java
@@ -10,9 +10,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -34,6 +44,10 @@ public class PlayScreen implements Screen{
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
 	
+	//Box2d variables
+	private World world;
+	private Box2DDebugRenderer b2dr;
+	
 	public PlayScreen(AngryDonkeyKongLibGDX game) {
 		this.game = game;
 		
@@ -53,6 +67,55 @@ public class PlayScreen implements Screen{
 		renderer = new OrthogonalTiledMapRenderer(map);
 		gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight()/2, 0);
 		
+		world = new World(new Vector2(0, 0), true);
+		b2dr = new Box2DDebugRenderer();
+		
+		BodyDef bdef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
+		FixtureDef fdef = new FixtureDef();
+		Body body;
+		
+		//create background
+		for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()/2);
+			
+			body = world.createBody(bdef);
+			
+			shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		
+		//create ground
+		for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()/2);
+			
+			body = world.createBody(bdef);
+			
+			shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		
+		//create platform
+		for(MapObject object : map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()/2);
+			
+			body = world.createBody(bdef);
+			
+			shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
 	}
 	
 	@Override
@@ -88,6 +151,8 @@ public class PlayScreen implements Screen{
 //		game.batch.draw(img, -451, -295);
 //		game.batch.end();
 	
+		b2dr.render(world, gamecam.combined);
+		
 		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
 		hud.stage.draw();
 		
