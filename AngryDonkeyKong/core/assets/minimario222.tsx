a7cff18d56f15fb591148adaafca52500e4d9a92
Sean Benson <seanalexanderbenson@gmail.com>
2017-10-01 17:25:50 -0500

    Map works!

--- AngryDonkeyKong/core/assets/minimario222.tsx
+++ AngryDonkeyKong/core/assets/minimario222.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="minimairo222" tilewidth="16" tileheight="16" tilecount="3" columns="3">
- <image source="eBooks/mini_maro_spritesheet.png" trans="005b7f" width="48" height="16"/>
+ <image source="mini_maro_spritesheet.png" trans="005b7f" width="48" height="16"/>
 </tileset>
